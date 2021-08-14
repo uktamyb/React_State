53a806dbd5ab24bcc3f93e00fd932a5255e3b4ee
UktamHudoyaorv <uktamyb@gmail.com>
2021-08-14 18:37:30 +0500

    hellow

--- src/App.jsx
+++ src/App.jsx
@@ -5,79 +5,28 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
-        { id: 1, name: "Uktam", status: "Hight" },
-        { id: 2, name: "Barkamol", status: "Low" },
-        { id: 3, name: "Abror", status: "Very high" },
-        { id: 4, name: "Ikrom", status: "Very low" },
-      ],
+      isLoggedIn: true,
     };
   }
   render() {
-    const onDelete = (id) => {
-      console.log("deleted", id);
-      const newData = this.state.data.filter(value => value.id !== id);
-      this.setState({ data: newData })
-    };
-
-    const onEdit = (id, e) => {
-      console.log("edeted", id);
-      this.setState({ data: e.target.value })
-      // const newData = this.state.data.filter(value => value.id !== id);
-      // this.setState({ data: newData })
-    };
-
-    const onAdd = () => {
-      console.log(this.state.name, this.state.status);
-      const newData = [
-        ...this.state.data,
-        { id: this.state.data.length + 1, name: this.state.name, status: this.state.status },
-      ]
-      this.setState({ data: newData })
-    };
-
-    const onChangeName = (e) => {
-      this.setState({ name: e.target.value })
-    };
-
-    const onChangeStatus = (e) => {
-      this.setState({ status: e.target.value })
-    };
-
-    return (
-      <div>
-        <input onChange={onChangeName} type="text" placeholder="name" />
-        <input onChange={onChangeStatus} type="text" placeholder="status" />
-        <button onClick={onAdd}>add</button>
-        <table border="1" style={{ borderCollapse: "collapse" }}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              this.state.data.map((value) => {
-                return (
-
-                  <tr key={value.id}>
-                    <td>{value.id}</td>
-                    <td>{value.name}</td>
-                    <td>{value.status}</td>
-                    <td><button onClick={() => onEdit(value.id)}>edit</button></td>
-                    <td><button onClick={() => onDelete(value.id)}>delete</button></td>
-                  </tr>
-
-
-                );
-              })
-            }
-          </tbody>
-        </table>
-      </div >
-    );
+    const onChange = () => {
+      this.setState({ isLoggedIn: !this.state.isLoggedIn })
+    }
+    if (this.state.isLoggedIn)
+      return (
+        <div>
+          <h1>Welcome to facebook</h1>
+          <button onClick={onChange}>hi to facebook</button>
+        </div >
+      );
+    else
+      return (
+        <div>
+          <h1>
+            Bye to facebook
+          </h1>
+          <button onClick={onChange}>bye to facebook</button>
+        </div>
+      )
   }
 }
---
+++ src/table.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      status: "",
+      data: [
+        { id: 1, name: "Uktam", status: "Hight" },
+        { id: 2, name: "Barkamol", status: "Low" },
+        { id: 3, name: "Abror", status: "Very high" },
+        { id: 4, name: "Ikrom", status: "Very low" },
+      ],
+    };
+  }
+  render() {
+
+
+    const onDelete = (id) => {
+      console.log("deleted", id);
+      const newData = this.state.data.filter(value => value.id !== id);
+      this.setState({ data: newData })
+    };
+
+    const onAdd = () => {
+      console.log(this.state.name, this.state.status);
+      const newData = [
+        ...this.state.data,
+        { id: this.state.data.length + 1, name: this.state.name, status: this.state.status },
+      ]
+      this.setState({ data: newData })
+    };
+
+    const onChangeName = (e) => {
+      this.setState({ name: e.target.value })
+    };
+
+    const onChangeStatus = (e) => {
+      this.setState({ status: e.target.value })
+    };
+
+
+
+    return (
+      <div>
+        <input onChange={onChangeName} type="text" placeholder="name" />
+        <input onChange={onChangeStatus} type="text" placeholder="status" />
+        <button onClick={onAdd}>add</button>
+        <table border="1" style={{ borderCollapse: "collapse" }}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.data.map((value) => {
+                return (
+
+                  <tr key={value.id}>
+                    <td>{value.id}</td>
+                    <td>{value.name}</td>
+                    <td>{value.status}</td>
+                    <td><button onClick={() => onDelete(value.id)}>delete</button></td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+        {/* <button onClick={getSortByName}>Sort by name</button> */}
+      </div >
+    );
+  }
+}
